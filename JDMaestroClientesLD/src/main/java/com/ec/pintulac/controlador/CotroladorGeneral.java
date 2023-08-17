82b1051c4261b47f35d4b6964346cd200fba4f52
DarwinGato1411 <darwinvinicio14@gmail.com>
2023-08-17 12:10:12 -0500

    Darwin

--- JDMaestroClientesLD/src/main/java/com/ec/pintulac/controlador/CotroladorGeneral.java
+++ JDMaestroClientesLD/src/main/java/com/ec/pintulac/controlador/CotroladorGeneral.java
@@ -1,25 +1,14 @@
 package com.ec.pintulac.controlador;
-import java.util.Base64;
-
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import com.ec.pintulac.repository.RepositoryGenerico;
-import com.ec.pintulac.request.MaestroClientesLDRequest;
-
 import com.ec.pintulac.response.MaestroClientesLDResponse;
 import com.ec.pintulac.services.ServicioGeneral;
 import com.google.gson.Gson;
@@ -50,31 +39,33 @@ public class CotroladorGeneral {
 	@Value("${password.token}")
 	private String passwordToken;
 
-//	@PostMapping(value = "/consultas_mestroInventario")
-//	@ApiOperation(tags = "ConsultasMestro de Inventarios", value = "ConsultasMestro de Inventarios")
-//	public ResponseEntity<?> obtenerConsultasCostos2(@RequestBody MaestroInventariosRequest requestBody) {
+	
+//	
+//	@PostMapping(value = "/maestro_clientes_est")
+//	@ApiOperation(tags = "maestro_clientes_LD2", value = "Detallar las integraciones que componen la interfaz de salida de maestro de clientes en el libro de direcciones.")
+//	public MaestroClientesLDResponse obtener(@RequestBody MaestroClientesLDRequest param) {
 //		try {
 //			HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
 //					HttpClientBuilder.create().build());
 //			RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
 //
-//			String authStr = "JDEDIS1:JDEDIS2";
+//			String authStr = "JDESIF:JDESIF";
 //			String base64Creds = Base64.getEncoder().encodeToString(authStr.getBytes());
 //
 //			// create headers
 //			HttpHeaders headers = new HttpHeaders();
 //			headers.add("Authorization", "Basic " + base64Creds);
 //
-//			HttpEntity<MaestroInventariosRequest> requestEntity = new HttpEntity<>(requestBody, headers);
-//			ResponseEntity<MaestroInventarioResponse> response = restTemplate.exchange(ruta, HttpMethod.POST,
-//					requestEntity, MaestroInventarioResponse.class);
+//			HttpEntity<MaestroClientesLDRequest> requestEntity = new HttpEntity<>(param, headers);
+//			ResponseEntity<MaestroClientesLDResponse> response = restTemplate.exchange(ruta, HttpMethod.POST,
+//					requestEntity, MaestroClientesLDResponse.class);
 //
 //			HttpStatus statusCode = response.getStatusCode();
 //
 //			if (statusCode.is2xxSuccessful()) {
-//				MaestroInventarioResponse consultasCostosResponse = response.getBody();
+//				MaestroClientesLDResponse consultasCostosResponse = response.getBody();
 //				
-//				return ResponseEntity.ok(consultasCostosResponse);
+//				return consultasCostosResponse;
 //			} else {
 //				System.err.println("Error al hacer la solicitud. Código de respuesta: " + statusCode.value());
 //				return null;
@@ -82,13 +73,7 @@ public class CotroladorGeneral {
 //
 //		}
 //
-//		catch (HttpClientErrorException | HttpServerErrorException ex) {
-//			// Catch specific exceptions for handling errors
-//			System.err.println("Error during API request: " + ex.getMessage());
-//			// Handle the error response here
-//			// You can get the error response body using ex.getResponseBodyAsString()
-//			return ResponseEntity.status(ex.getStatusCode()).body("Por favor revise los datos ingresados"+ResponseEntity.status(400));
-//		}
+//
 //
 //		catch (Exception ex) {
 //			ex.printStackTrace();
@@ -97,77 +82,30 @@ public class CotroladorGeneral {
 //
 //	}
 	
-//	@PostMapping(value = "/consultas_envios")
-//	@ApiOperation(tags = "Consultas envios", value = "Detallar integración para salida de envíos")
-//	public ResponseEntity<?> consultas_mestroInventario(@RequestBody ConnectorRequest1Data param) {
-//		try {
-//			long totalSum = 0;
-//			long startTime = System.currentTimeMillis();
-//			int i = 0;
-//			JsonObject respuesta = new JsonObject();
-////			ExistenciaFisicaResponse JSONJDE = servicioGeneral.invocarJDE(param);
-//			System.out.println("NUM ELEMENTOS: " + param.getItem().size());
-////			for (Row item : JSONJDE.getConnectorRequest1().getRows()) {
-//				Gson gson = new Gson();
-//				String JSON = gson.toJson(param);
-//				respuesta = generico.callStoreProcedureArray("DINAMIC.sp_jsonin_maestro_inventario ", JSON);
-//				System.out.println(i++ + ": " + respuesta);
-////			}
-//			totalSum = (System.currentTimeMillis() - startTime);
-//			System.out.println("Tiempo ejecucion" + (totalSum / 1000));
-//			return new ResponseEntity<String>(respuesta.toString(), HttpStatus.OK);
-////			
-//
-//		} catch (Exception ex) {
-//			ex.printStackTrace();
-//			return null;
-//		}
-//
-//	}
 	
-	
-	
-	@PostMapping(value = "/maestro_clientes_LD2")
-	@ApiOperation(tags = "maestro_clientes_LD2", value = "Detallar las integraciones que componen la interfaz de salida de maestro de clientes en el libro de direcciones.")
-	public MaestroClientesLDResponse obtener(@RequestBody MaestroClientesLDRequest param) {
+	@RequestMapping(value = "/maestro_clientes_LD2", method = RequestMethod.POST)
+	@ApiOperation(tags = "maestro_clientes_LD2", value = "Registrar o actulizar marcas")
+	public ResponseEntity<?> maestroClientes(@RequestBody MaestroClientesLDResponse param) {
+
 		try {
-			HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
-					HttpClientBuilder.create().build());
-			RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+			long totalSum = 0;
+			long startTime = System.currentTimeMillis();
+			int i = 0;
+			JsonObject respuesta = new JsonObject();
+//			ExistenciaFisicaResponse JSONJDE = servicioGeneral.invocarJDE(param);
+			
+//			for (Row item : JSONJDE.getConnectorRequest1().getRows()) {
+				Gson gson = new Gson();
+				String JSON = gson.toJson(param);
+				respuesta = generico.callStoreProcedureArray("DINAMIC.sp_json3_existencia", JSON);
+				System.out.println(i++ + ": " + respuesta);
+//			}
+			totalSum = (System.currentTimeMillis() - startTime);
+			System.out.println("Tiempo ejecucion" + (totalSum / 1000));
+			return new ResponseEntity<String>(respuesta.toString(), HttpStatus.OK);
+//			
 
-			String authStr = "JDESIF:JDESIF";
-			String base64Creds = Base64.getEncoder().encodeToString(authStr.getBytes());
-
-			// create headers
-			HttpHeaders headers = new HttpHeaders();
-			headers.add("Authorization", "Basic " + base64Creds);
-
-			HttpEntity<MaestroClientesLDRequest> requestEntity = new HttpEntity<>(param, headers);
-			ResponseEntity<MaestroClientesLDResponse> response = restTemplate.exchange(ruta, HttpMethod.POST,
-					requestEntity, MaestroClientesLDResponse.class);
-
-			HttpStatus statusCode = response.getStatusCode();
-
-			if (statusCode.is2xxSuccessful()) {
-				MaestroClientesLDResponse consultasCostosResponse = response.getBody();
-				
-				return consultasCostosResponse;
-			} else {
-				System.err.println("Error al hacer la solicitud. Código de respuesta: " + statusCode.value());
-				return null;
-			}
-
-		}
-
-//		catch (HttpClientErrorException | HttpServerErrorException ex) {
-//			// Catch specific exceptions for handling errors
-//			System.err.println("Error during API request: " + ex.getMessage());
-//			// Handle the error response here
-//			// You can get the error response body using ex.getResponseBodyAsString()
-//			return ResponseEntity.status(ex.getStatusCode()).body("Por favor revise los datos ingresados"+ResponseEntity.status(400));
-//		}
-
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return null;
 		}
